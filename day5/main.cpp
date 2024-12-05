3f7c091c4cc5fb1db9d99fa171ffed2952a6a75e
David Mirabito <1133440+DaveJMir@users.noreply.github.com>
2024-12-05 17:03:23 +1100

    Day5: ⭐️
    so far, work in progress

---
+++ day5/main.cpp
@@ -0,0 +1,109 @@
+
+#include "util/util.hpp"
+
+#include <algorithm>
+#include <numeric>
+#include <iostream>
+#include <ranges>
+
+#include <utility>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <iostream>
+#include <cassert>
+
+using namespace aoc::util;
+
+std::vector<int> splitStringBy(const std::string& str, char c)
+{
+  std::vector<int> ret{};
+
+  std::stringstream ss{str};
+  std::string tmp;
+
+  while (getline(ss, tmp, c))
+    ret.push_back(std::stoi(tmp));
+
+  return ret;
+}
+
+bool obeys_constraint(const std::vector<int>& update, std::pair<int,int> rule)
+{
+  auto firstIter = std::find(update.begin(), update.end(), rule.first);
+  auto secondIter = std::find(update.begin(), update.end(), rule.second);
+
+  if(firstIter == update.end() || secondIter == update.end())
+    return true;
+
+  auto firstIdx = firstIter - update.begin();
+  auto secondIdx = secondIter - update.begin();
+
+  return firstIdx < secondIdx;
+}
+
+// first is middle when correct
+// second is middle once fixed
+// probably both wont be non-zero?
+std::pair<int,int> validate_update(const std::vector<int>& update, const std::vector<std::pair<int,int>>& rules)
+{
+  for( const auto& rule : rules)
+  {
+    if(!obeys_constraint(update, rule))
+    {
+      return std::make_pair(0,0);
+    }
+  }
+  return std::make_pair(update[update.size()/2], 0);
+}
+
+
+int main() {
+  #if 1
+  std::ifstream input{"input.txt"};
+  #else
+  std::ifstream input{"example.txt"};
+  #endif
+
+  std::vector<std::pair<int,int>> rules{};
+  std::vector<std::vector<int>> updates{};
+  bool processingRules = true;
+
+  std::string line;
+  while(std::getline(input, line)) {
+    auto payload = trimmed(line);
+    if(payload.empty()) 
+    {
+      processingRules = false;
+      continue;
+    }
+
+    if(processingRules){
+      //  std::cout << "Rule: " << line << "\n";
+       auto ruleVec = splitStringBy({line}, '|');
+       rules.emplace_back(ruleVec[0], ruleVec[1]);
+    } else {
+      //  std::cout << "Update: " << line << "\n";
+       updates.emplace_back(splitStringBy({line}, ','));
+    }
+  }
+
+  // std::cout << "havbe " << rules.size() << "rules and " << updates.size() << "updates\n";
+
+  int part1 = 0;
+  int part2 = 0;
+  for(const auto& update : updates)
+  {
+    auto result = validate_update(update, rules);
+    part1 += result.first;
+  }
+
+
+  std::cout << "Part1: " << part1 << "\n";
+  std::cout << "Part2: " << part2 << "\n";
+
+
+
+  return 0;
+}
