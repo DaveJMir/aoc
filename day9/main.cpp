7dcc1be945dde18127ec15f726e6052d81883223
David Mirabito <1133440+DaveJMir@users.noreply.github.com>
2024-12-09 22:50:30 +1100

    Day9: ⭐️🌟
    I had grander plans, but it works.
    Could keep a feeList of empty blocks, but then I'd have to split it up
    on partial allocations and re-insert according to remaining size, if
    any. But this is fast enough, so ok.
    
    Also lots of gross mixing of iterator and index math.

--- day9/main.cpp
+++ day9/main.cpp
@@ -3,23 +3,18 @@
 #include <algorithm>
 #include <cassert>
 #include <expected>
-#include <fstream>
-#include <iostream>
-#include <iterator>
-#include <map>
-#include <random>
-#include <numeric>
-#include <ranges>
 #include <vector>
-#include <ranges>
 #include <string_view>
 
 using namespace aoc::util;
 
 struct Block
 {
-  Block(int id) : fileId{id} {}
+  Block(int id, int num, int total) : fileId{id}, blockNum{num}, totalBlocks{total} {}
   int fileId; // or -1 for freespace
+  int blockNum;
+  int totalBlocks;
+
 
   bool isEmpty() const { return fileId == -1; }
 
@@ -34,43 +29,92 @@ struct Block
 
 struct Hdd
 {
+  using BlockList = std::vector<Block>;
   explicit Hdd(std::ifstream&& input)
   {
-
     int lineCount = foreach_line(input, [this](std::string_view line)
     {
       bool isFile{true};
       for(auto c : line)
       {
-        int fileId = (isFile)? fileCount++ : -1;
+        int fileId = (isFile)? fileMap.size() : -1;
+        if(isFile)
+          fileMap.push_back(blockList.size());
+        int totalBlocks = c - '0';
 
-        for( int i=0; i<c-'0'; i++)
+        for( int i=0; i<totalBlocks; i++)
         {
-          blockList.emplace_back(fileId);
+          blockList.emplace_back(fileId, i, totalBlocks);
         }
         isFile = !isFile;
       }
 
     });
-    assert(lineCount == 1);
+    assert(lineCount == 1 && "Only expect one line of input");
   }
 
-  void defrag()
+  void refrag()
   {
-
     auto forwardEmpty = std::find_if(blockList.begin(), blockList.end(),
                                      [](auto &b) { return b.isEmpty(); });
     auto reverseFile = std::find_if(blockList.rbegin(), blockList.rend(),
                                     [](auto &b) { return !b.isEmpty(); });
 
-    // Perform swapping
     while (forwardEmpty != blockList.end() && reverseFile != blockList.rend()) {
       if (reverseFile.base() <= forwardEmpty) {
         break;
       }
-        std::swap(*forwardEmpty, *reverseFile);
-        for(++forwardEmpty; !forwardEmpty->isEmpty() && forwardEmpty!=blockList.end(); ++forwardEmpty);
-        for(++reverseFile; reverseFile->isEmpty() && reverseFile!=blockList.rend(); ++reverseFile);
+      std::swap(*forwardEmpty, *reverseFile);
+      for (++forwardEmpty;
+           forwardEmpty != blockList.end() && !forwardEmpty->isEmpty();
+           ++forwardEmpty)
+        ;
+      for (++reverseFile;
+           forwardEmpty != blockList.end() && reverseFile->isEmpty();
+           ++reverseFile)
+        ;
+    }
+  }
+
+  BlockList::iterator findFreeSpace(int blocksNeeded)
+  {
+    for(auto it = blockList.begin(); it != blockList.end(); ++it)
+    {
+      size_t count = 0;
+      while(it->isEmpty() && it!=blockList.end())
+      {
+        count++;
+        it++;
+      }
+      if(count >= blocksNeeded)
+      {
+        return it - count;
+      }
+      if(it == blockList.end())
+        break;
+    }
+    return blockList.end();
+  }
+
+  void moveFile(BlockList::iterator source, BlockList::iterator dest)
+  {
+    int size = source->totalBlocks;
+    for(int i=0; i<size; i++)
+    {
+      std::swap(*source, *dest);
+      ++source;
+      ++dest;
+    }
+  }
+
+  void defrag() {
+    for (auto blockIdx : std::ranges::views::reverse(fileMap)) {
+      auto block = blockList.begin() + blockIdx;
+      auto freeSpace = findFreeSpace(block->totalBlocks);
+      if (freeSpace == blockList.end() || freeSpace > block) {
+        continue;
+      }
+      moveFile(block, freeSpace);
     }
   }
 
@@ -88,8 +132,8 @@ struct Hdd
     return ret;
   }
 
-  std::vector<Block> blockList;
-  int fileCount{0};
+  BlockList blockList;
+  std::vector<std::size_t> fileMap;
 
   friend std::ostream &operator<<(std::ostream &os, const Hdd &h) {
     os << "Hdd{";
@@ -98,14 +142,15 @@ struct Hdd
   }
 };
 
-
 std::pair<std::uint64_t, std::uint64_t> process(std::ifstream&& input)
 {
   assert(input.is_open());
-  Hdd hdd{std::move(input)};
-  hdd.defrag();
+  Hdd hdd_1{std::move(input)};
+  Hdd hdd_2 = hdd_1;
+  hdd_1.refrag();
+  hdd_2.defrag();
   std::pair<std::uint64_t, std::uint64_t>{};
-  return {hdd.checksum(), 0};
+  return {hdd_1.checksum(), hdd_2.checksum()};
 }
 
 int main() {
