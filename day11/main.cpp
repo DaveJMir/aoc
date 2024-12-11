27e8f136159063799a020562d6edd27a94b4d3d3
David Mirabito <1133440+DaveJMir@users.noreply.github.com>
2024-12-11 17:32:51 +1100

    Day11: ⭐️🌟
    So while the literal interpretation of the question (modelling rows of
    stones as actual vector of numbers) may work for 25 iterations, it
    absolutely does not for 75. I got killed at something like 180GB or RAM
    usage.
    
    Noting that no number depends on any other, we can just see how many
    stones the first one expands to for a given generation, and add that to
    how many the next does. This way we don't keep all 64-200 trillion
    numbers at once. Turns out this is still super slow, even though memory
    use is under control.
    
    So we just slap a memoization layer in front and it suddenly becomes
    super tractable: 50ms to do part1 and part1 of example and my real
    input!

--- day11/main.cpp
+++ day11/main.cpp
@@ -9,25 +9,24 @@
 #include <map>
 #include <ranges>
 #include <set>
-#include <ranges>
 #include <string_view>
 
 using namespace aoc::util;
 
-using Val = uint32_t;
+using Val = uint64_t;
+using MemKey = std::pair<Val, int>;
+using Memoize = std::map<MemKey, Val>;
 
-void pv(auto& t)
-{
+void pv(auto &t) {
   return;
   std::cout << "{";
-  for(auto x : t) std::cout << " " << x;
+  for (auto x : t)
+    std::cout << " " << x;
   std::cout << " }\n";
 }
 
-
-int numDigits(Val n)
-{
-  int ret=0;
+int numDigits(Val n) {
+  int ret = 0;
   while (n) {
     ret++;
     n /= 10;
@@ -35,48 +34,48 @@ int numDigits(Val n)
   return ret;
 }
 
-std::pair<uint64_t, uint64_t> split(Val s, int num)
-{
-  int scale = std::pow(10, num/2);
-  return {s / scale, s%scale };
+std::pair<uint64_t, uint64_t> split(Val s, int num) {
+  int scale = std::pow(10, num / 2);
+  return {s / scale, s % scale};
 }
 
-std::uint64_t stoneBlink(std::vector<Val>&& stones, int generation)
-{
-  std::cout << "generation: " << generation << "[" << stones.size() <<  "]\n";
-  if(generation==0) return stones.size();
-
-  std::vector<Val> next{};
-
-  for(auto s : stones)
-  {
-    if(s==0) next.push_back(1);
-    else if(int numdigits = numDigits(s); (numdigits & 1) == 0)
-    {
-       auto vals = split(s, numdigits);
-       next.push_back(vals.first);
-       next.push_back(vals.second);
-    }
-    else next.push_back(s*2024);
+Val stoneBlink(const Val s, int generation, Memoize &memoize) {
+  if (generation == 0)
+    return 1;
+  auto key = MemKey{s, generation};
+  if (auto found = memoize.find(key); found != memoize.end()) {
+    return found->second;
   }
-  pv(next);
 
+  Val ret;
+  if (s == 0)
+    ret = stoneBlink(1, generation - 1, memoize);
+  else if (int numdigits = numDigits(s); (numdigits & 1) == 0) {
+    auto vals = split(s, numdigits);
+    ret = stoneBlink(vals.first, generation - 1, memoize) +
+          stoneBlink(vals.second, generation - 1, memoize);
+  } else
+    ret = stoneBlink(s * 2024, generation - 1, memoize);
 
-  return stoneBlink(std::move(next), generation-1);
+  memoize[key] = ret;
+  return ret;
 }
 
-
-std::pair<std::uint64_t, std::uint64_t> process(std::ifstream&& input)
-{
+std::pair<std::uint64_t, std::uint64_t> process(std::ifstream &&input) {
   assert(input.is_open());
 
+  Memoize memoize_p1{};
+  Memoize memoize_p2{};
+
   std::pair<std::uint64_t, std::uint64_t> result{};
-  int numLines = foreach_line(input, [&result](std::string_view line)
-  {
-    auto vals = string2vec<Val>(line);
-    pv(vals);
-    result.first += stoneBlink(std::move(vals), 75);
-  });
+  int numLines = foreach_line(
+      input, [&result, &memoize_p1, &memoize_p2](std::string_view line) {
+        auto vals = string2vec<Val>(line);
+        for (auto v : vals) {
+          result.first += stoneBlink(v, 25, memoize_p1);
+          result.second += stoneBlink(v, 75, memoize_p2);
+        }
+      });
   assert(numLines == 1);
 
   return result;
