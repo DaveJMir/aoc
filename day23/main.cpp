fc6ff7103f71af02c9b6550b019f92df9abb0808
David Mirabito <1133440+DaveJMir@users.noreply.github.com>
2024-12-24 12:45:19 +1100

    Day23 prep

---
+++ day23/main.cpp
@@ -0,0 +1,22 @@
+#include "util/util.hpp"
+
+#include <algorithm>
+#include <cassert>
+#include <expected>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <ranges>
+#include <set>
+#include <ranges>
+
+using namespace aoc::util;
+
+
+std::pair<std::uint64_t, std::uint64_t> process(std::ifstream&& input)
+{
+  assert(input.is_open());
+  std::pair<std::uint64_t, std::uint64_t> ret{};
+  return ret;
+}
